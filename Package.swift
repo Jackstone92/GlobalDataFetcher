3bfd32e943ded6d48ac38bd57c3b10b49b245bed
Jack <jackstonemedia@gmail.com>
2022-02-21 19:54:26 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -39,6 +39,36 @@ package.targets.append(contentsOf: [
     ),
 ])
 
+// MARK: - Features
+package.products.append(contentsOf: [
+    .library(
+        name: "DataFetchFeature",
+        targets: ["DataFetchFeature"]
+    )
+])
+
+package.targets.append(contentsOf: [
+    .target(
+        name: "DataFetchFeature",
+        dependencies: [
+            .product(name: "RxSwift", package: "RxSwift"),
+            .product(name: "RxCocoa", package: "RxSwift"),
+            .product(name: "RxAnimated", package: "RxAnimated"),
+            "StyleGuide",
+            "ContentService",
+            "CounterService",
+            "LastResponseCodeService"
+        ]
+    ),
+    .testTarget(
+        name: "DataFetchFeatureTests",
+        dependencies: [
+            "DataFetchFeature",
+            "TestSchedulers"
+        ]
+    )
+])
+
 // MARK: - Stores
 package.products.append(contentsOf: [
     .library(
@@ -82,4 +112,113 @@ package.targets.append(contentsOf: [
         name: "CounterStoreTests",
         dependencies: ["CounterStoreLive"]
     ),
+])
+
+// MARK: - Clients and services
+package.products.append(contentsOf: [
+    .library(
+        name: "NetworkClient",
+        targets: ["NetworkClient"]
+    ),
+    .library(
+        name: "ContentServiceLive",
+        targets: ["ContentServiceLive"]
+    ),
+    .library(
+        name: "CounterServiceLive",
+        targets: ["CounterServiceLive"]
+    ),
+    .library(
+        name: "LastResponseCodeServiceLive",
+        targets: ["LastResponseCodeServiceLive"]
+    ),
+])
+
+package.targets.append(contentsOf: [
+    .target(
+        name: "NetworkClient",
+        dependencies: [
+            .product(name: "RxSwift", package: "RxSwift"),
+            .product(name: "RxCocoa", package: "RxSwift")
+        ]
+    ),
+    .testTarget(
+        name: "NetworkClientTests",
+        dependencies: ["NetworkClient", "NetworkClientTestSupport"]
+    ),
+
+    .target(
+        name: "ContentService",
+        dependencies: [.product(name: "RxSwift", package: "RxSwift")]
+    ),
+    .target(
+        name: "ContentServiceLive",
+        dependencies: ["ContentService", "NetworkClient"]
+    ),
+    .testTarget(
+        name: "ContentServiceTests",
+        dependencies: [
+            .product(name: "RxTest", package: "RxSwift"),
+            "ContentServiceLive",
+            "TestSchedulers",
+            "NetworkClientTestSupport"
+        ]
+    ),
+
+    .target(
+        name: "CounterService",
+        dependencies: []
+    ),
+    .target(
+        name: "CounterServiceLive",
+        dependencies: ["CounterService", "CounterStore"]
+    ),
+    .testTarget(
+        name: "CounterServiceTests",
+        dependencies: ["CounterServiceLive"]
+    ),
+
+    .target(
+        name: "LastResponseCodeService",
+        dependencies: []
+    ),
+    .target(
+        name: "LastResponseCodeServiceLive",
+        dependencies: [
+            "LastResponseCodeService",
+            "ResponseCodeStore"
+        ]
+    ),
+    .testTarget(
+        name: "LastResponseCodeServiceTests",
+        dependencies: ["LastResponseCodeServiceLive"]
+    ),
+])
+
+// MARK: - Test support
+package.products.append(contentsOf: [
+    .library(
+        name: "NetworkClientTestSupport",
+        targets: ["NetworkClientTestSupport"]
+    )
+])
+
+package.targets.append(contentsOf: [
+    .target(
+        name: "NetworkClientTestSupport",
+        dependencies: []
+    ),
+    .testTarget(
+        name: "NetworkClientTestSupportTests",
+        dependencies: ["NetworkClientTestSupport"]
+    ),
+
+    .target(
+        name: "TestSchedulers",
+        dependencies: [.product(name: "RxSwift", package: "RxSwift")]
+    ),
+    .testTarget(
+        name: "TestSchedulersTests",
+        dependencies: ["TestSchedulers"]
+    )
 ])
